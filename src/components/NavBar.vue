56cd8fd9412a4c21a608b71b97797aa14e56e153
Felipe Richter <frichterc@gmail.com>
2024-11-29 19:17:35 -0300

    feat: add header

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="p-4 bg-primary">
+    <RouterLink to="/" class="hover:cursor-pointer">
+      <div class="text-lg font-bold text-primary-content">PokeDesafío</div>
+    </RouterLink>
+  </nav>
+</template>
