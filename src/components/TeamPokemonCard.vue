6b75c749800df8546aa0f9444aa56bd391612f8e
Felipe Richter <frichterc@gmail.com>
2024-11-30 19:11:09 -0300

    feat: add basic team route view

---
+++ src/components/TeamPokemonCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Pokemon } from "@/types";
+
+const props = defineProps<{ pokemon: Pokemon }>();
+</script>
+
+<template>
+  <div class="bg-neutral text-neutral-content rounded p-4 mt-6 flex relative">
+    <div class="absolute -top-6 object-cover">
+      <img :src="pokemon.sprite" />
+    </div>
+    <div class="uppercase text-sm font-bold mt-10 w-32">
+      #{{ pokemon.id }}
+      <div>{{ pokemon.name }}</div>
+    </div>
+
+    <div class="flex">
+      <div class="uppercase text-xs">Types</div>
+      <div v-for="type in pokemon.types">
+        {{ type }}
+      </div>
+    </div>
+  </div>
+</template>
