b79158ba5b38af0d85c2cdab0a291a277b5c429d
Felipe Richter <frichterc@gmail.com>
2024-11-30 10:57:24 -0300

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-import NavBar from "./components/NavBar.vue"
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
   <header>
     <NavBar />
   </header>
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
+  <footer class="bg-neutral text-neutral-content mb-20 py-4 text-center">
+    Felipe Richter &copy; {{ new Date().getFullYear() }}
+  </footer>
 </template>
