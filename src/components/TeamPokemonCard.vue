1631d7518aa52122acbd4ea1be3d56a629da8827
Felipe Richter <frichterc@gmail.com>
2024-11-30 21:12:00 -0300

    feat: add types and sprites to team card

--- src/components/TeamPokemonCard.vue
+++ src/components/TeamPokemonCard.vue
@@ -1,23 +1,41 @@
 <script setup lang="ts">
 import type { Pokemon } from "@/types";
+import { computed } from "vue";
 
 const props = defineProps<{ pokemon: Pokemon }>();
+
+const typeColors = computed(() => {
+  return props.pokemon.types.map((type) => "bg-poke-" + type);
+});
 </script>
 
 <template>
-  <div class="bg-neutral text-neutral-content rounded p-4 mt-6 flex relative">
-    <div class="absolute -top-6 object-cover">
-      <img :src="pokemon.sprite" />
-    </div>
-    <div class="uppercase text-sm font-bold mt-10 w-32">
-      #{{ pokemon.id }}
-      <div>{{ pokemon.name }}</div>
+  <div
+    class="bg-neutral text-neutral-content rounded rounded-l-full p-4 mt-6 flex relative items-start"
+  >
+    <div class="z-10">
+      <div class="absolute -top-5 left-10">
+        <img :src="pokemon.sprite" class="scale-125 blur-xl absolute" />
+        <img :src="pokemon.sprite" class="scale-125" />
+      </div>
+      <div class="uppercase text-sm font-bold mt-10 ml-4 w-32">
+        #{{ pokemon.id }}
+        <div>{{ pokemon.name }}</div>
+      </div>
     </div>
 
-    <div class="flex">
-      <div class="uppercase text-xs">Types</div>
-      <div v-for="type in pokemon.types">
-        {{ type }}
+    <div class="flex flex-col gap-1 font-mono">
+      <!-- Types -->
+      <div class="flex items-center">
+        <p class="uppercase text-xs mr-2">Types</p>
+        <div v-for="(type, i) in pokemon.types" :key="i">
+          <div
+            class="uppercase rounded-sm text-xs px-1 mr-1"
+            :class="typeColors[i]"
+          >
+            {{ type }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
