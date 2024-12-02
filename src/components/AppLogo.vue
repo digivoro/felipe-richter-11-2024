69c9a45e0df41310968c831781ad31af4501e380
Felipe Richter <frichterc@gmail.com>
2024-12-02 07:12:07 -0300

    improvement: tweak logo

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -3,11 +3,13 @@ import IconPokeballSquare from "./icons/IconPokeballSquare.vue";
 </script>
 
 <template>
-  <div
-    class="flex items-center justify-center font-extralight text-base-100 text-3xl uppercase tracking-tighter font-sans"
-  >
-    <span class="underline underline-offset-4 w-20 text-right">Poké</span>
-    <IconPokeballSquare class="size-12 mx-1" />
-    <span class="underline underline-offset-4 w-20 text-left">Team</span>
+  <div class="flex items-center justify-center font-extralight text-base-100">
+    <IconPokeballSquare class="size-20 mx-1" />
+    <div
+      class="flex flex-col w-20 text-3xl uppercase tracking-tighter font-mono"
+    >
+      <span class="font-black">Poké</span>
+      <span class="font-bold">Team</span>
+    </div>
   </div>
 </template>
