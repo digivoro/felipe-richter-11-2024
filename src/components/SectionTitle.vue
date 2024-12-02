4778a5d60bddcb9c04acc14def6deb9ec2b0076f
Felipe Richter <frichterc@gmail.com>
2024-12-02 08:34:57 -0300

    feat: update section title

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -3,12 +3,16 @@ import IconPokeball from "./icons/IconPokeball.vue";
 </script>
 
 <template>
-  <div
-    class="flex items-center gap-1 bg-neutral rounded mb-4 px-2 shadow-md shadow-[rgba(0,0,0,0.4)]"
-  >
-    <IconPokeball class="size-4 text-secondary" />
-    <h2 class="text-lg uppercase font-black text-neutral-content">
-      <slot />
-    </h2>
+  <div class="flex items-center mb-4 relative">
+    <IconPokeball
+      class="size-12 text-neutral absolute -top-5 -z-10 left-0 -rotate-12"
+    />
+    <div
+      class="flex items-center gap-1 bg-secondary rounded px-2 py-1 ml-6 shadow-md shadow-[rgba(0,0,0,0.3)]"
+    >
+      <h2 class="text-lg uppercase font-black text-secondary-content">
+        <slot />
+      </h2>
+    </div>
   </div>
 </template>
