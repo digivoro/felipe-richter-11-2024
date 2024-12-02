5678ed1e17ea1e5f38e67201d2e81853b4338884
Felipe Richter <frichterc@gmail.com>
2024-12-02 06:54:21 -0300

    feat: add type icon component

---
+++ src/components/icons/IconType.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const { type } = defineProps<{
+  type: string;
+}>();
+
+const url = computed(() => `/src/assets/icons/pokemon-types/${type}.svg`);
+</script>
+
+<template>
+  <img :src="url" />
+</template>
