2927e36e3d9e62f39550510b24c496011d7f55dc
Felipe Richter <frichterc@gmail.com>
2024-12-02 06:56:15 -0300

    feat: add type badge

---
+++ src/components/TypeBadge.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { Pokemon } from "@/types";
+import IconType from "./icons/IconType.vue";
+
+const { type } = defineProps<{
+  type: Pokemon["types"][number];
+}>();
+</script>
+
+<template>
+  <div
+    class="uppercase rounded-sm text-xs px-1 py-[2px] font-medium flex items-center text-white"
+    :class="`bg-poke-${type}`"
+  >
+    <div class="mr-1 size-3">
+      <IconType :type="type" />
+    </div>
+    {{ type }}
+  </div>
+</template>
