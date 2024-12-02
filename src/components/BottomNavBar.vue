b9ea8714564a817413454802be3a4fbc22571b14
Felipe Richter <frichterc@gmail.com>
2024-12-02 07:16:22 -0300

    feat: update bottom navbar

--- src/components/BottomNavBar.vue
+++ src/components/BottomNavBar.vue
@@ -2,11 +2,9 @@
 import { usePokemonStore } from "@/stores/pokemon";
 import { storeToRefs } from "pinia";
 import IconPokeball from "./icons/IconPokeball.vue";
-import IconNext from "./icons/IconNext.vue";
-import PaginationButton from "./icons/PaginationButton.vue";
 
 const pokemonStore = usePokemonStore();
-const { myTeam } = storeToRefs(pokemonStore);
+const { myTeam, isMyTeamFull } = storeToRefs(pokemonStore);
 </script>
 
 <template>
@@ -20,6 +18,9 @@ const { myTeam } = storeToRefs(pokemonStore);
     <RouterLink
       to="/team"
       class="active:scale-95 active:shadow-lg active:shadow-[rgba(0,0,0,0.7)] shadow-lg shadow-[rgba(0,0,0,0.5)] transition-all"
+      :class="{
+        'animate-bounce': isMyTeamFull,
+      }"
     >
       <div
         class="flex items-center bg-error text-secondary-content px-4 py-2 h-10 rounded-t"
@@ -28,9 +29,9 @@ const { myTeam } = storeToRefs(pokemonStore);
           <IconPokeball
             v-for="n in 6"
             :key="n"
-            class="size-6 transition-all duration-300 ease-out"
+            class="size-6 transition-all duration-500 ease-out"
             :class="{
-              'text-base-100 opacity-100 rotate-12': n <= myTeam.length,
+              'text-base-100 opacity-100 animate-spin-slow': n <= myTeam.length,
               'opacity-50': n > myTeam.length,
             }"
           />
@@ -38,9 +39,9 @@ const { myTeam } = storeToRefs(pokemonStore);
       </div>
       <div class="h-1 bg-neutral"></div>
       <div
-        class="flex items-center font-sans tracking-tighter justify-center text-md font-medium uppercase bg-base-100 text-base-content px-4 py-1 rounded-b"
+        class="flex items-center justify-center text-md font-medium uppercase bg-base-100 text-base-content px-4 py-1 rounded-b"
       >
-        <p>Go to <span class="underline underline-offset-2">my team</span></p>
+        My team
       </div>
     </RouterLink>
 
