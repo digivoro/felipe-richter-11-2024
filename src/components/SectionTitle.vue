ff61ae158e8dff1d22534a922a28be436e9f8477
Felipe Richter <frichterc@gmail.com>
2024-12-02 07:18:54 -0300

    feat: add section title

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import IconPokeball from "./icons/IconPokeball.vue";
+</script>
+
+<template>
+  <div
+    class="flex items-center gap-1 bg-neutral rounded mb-4 px-2 shadow-md shadow-[rgba(0,0,0,0.4)]"
+  >
+    <IconPokeball class="size-4 text-secondary" />
+    <h2 class="text-lg uppercase font-black text-neutral-content">
+      <slot />
+    </h2>
+  </div>
+</template>
